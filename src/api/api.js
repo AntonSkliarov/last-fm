4dab2b7e41a58e6b2ed96ed0955c72302cd55bb8
Anton <office.sklyarov@gmail.com>
2020-12-10 17:56:36 +0200

    update api url

--- src/api/api.js
+++ src/api/api.js
@@ -1,3 +1,3 @@
-export const BASE_URL = 'http://ws.audioscrobbler.com/2.0/';
+export const BASE_URL = 'https://ws.audioscrobbler.com/2.0/';
 
 export const API_KEY = '5190711e40adb8688facb8a58d660e2d';
